74b841e23ec4f594006ea92595ad7976d027a8a5
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-12-10 10:27:33 +0800

    add basic socketio

--- app/src/main/java/org/openrmf/temifleetadapterbridge/MainActivity.kt
+++ app/src/main/java/org/openrmf/temifleetadapterbridge/MainActivity.kt
@@ -1,30 +1,34 @@
 package org.openrmf.temifleetadapterbridge
+import org.openrmf.temifleetadapterbridge.BuildConfig
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import org.openrmf.temifleetadapterbridge.databinding.ActivityMainBinding
-import android.widget.Toast
+import io.socket.client.IO
+import io.socket.client.Socket
 
-const val WEBVIEW_URL = "com.openrmf.WEBVIEW_URL";
+const val WEBVIEW_URL = "com.openrmf.WEBVIEW_URL"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mSocket: Socket
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mSocket = IO.socket(BuildConfig.FLEET_ADAPTER_WS_URL)
+        mSocket.connect()
     }
 
-    fun onConnect(v: View?) {
-        val intent = Intent(this, WebViewActivity::class.java);
+    fun emitWebViewIntent(v: View?) {
+        val intent = Intent(this, WebViewActivity::class.java)
         val url = "https://www.google.com"
-        intent.putExtra(WEBVIEW_URL, url);
+        intent.putExtra(WEBVIEW_URL, url)
         startActivity(intent)
     }
 }
